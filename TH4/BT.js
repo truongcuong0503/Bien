5f96e045535d7e2351ea23c78af2a6ce99b49561
Cường Trương <truongxuancuong0503@gmail.com>
2022-04-19 21:44:33 +0700

    [Thực hành] Sử dụng toán tử

---
+++ TH4/BT.js
@@ -0,0 +1,13 @@
+let inputWidth;
+    inputWidth = prompt("Enter the width");
+
+let inputHeight;
+    inputHeight = prompt("Enter the height"); 
+
+let width = parseInt(inputWidth);
+
+let height = parseInt(inputHeight);
+
+let area = width * height;
+
+document.write("The area is: " + area);
